649f46b544d2aeda93b1c34d2da6d0fcc8b26e21
Tanbouz <phoenixsts@gmail.com>
2014-01-10 23:17:32 +0200

    Ship model, custom overlay UI & speed meter
    *Ship model included in scene.
    *Ship model wing lines are now larger and smoother to look less edgy in
    game.
    * A user interface based on overlying cameras added. No effect on OnGUI
    system.
    * Speed meter built using new UI. Relies on currentSpeed get method.

---
+++ Assets/scripts/UI/speed.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class speed : MonoBehaviour {
+
+	private Player player;
+	private TextMesh speedText;
+
+	// Use this for initialization
+	void Start () {
+		GameObject playerObj = GameObject.Find("Player2");
+		if (playerObj != null) 
+		{
+			player = (Player)playerObj.GetComponent (typeof(Player));
+			speedText = (TextMesh)gameObject.GetComponent(typeof(TextMesh));
+		}
+		else 
+		{
+			player = null;
+			Debug.Log("UI Error: Speed counter: Player game object not found!");
+		}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player != null){
+			//Debug.Log();
+			int speedtmp = (int)Mathf.Round(player.currentSpeed);
+			speedText.text = speedtmp.ToString()+" α";
+		}
+
+	}
+}
---
+++ Assets/scripts/UI/sphere.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sphere : MonoBehaviour {
+
+	private Player player;
+	
+	// Use this for initialization
+	void Start () {
+		GameObject playerObj = GameObject.Find("Player2");
+		if (playerObj != null) 
+		{
+			player = (Player)playerObj.GetComponent (typeof(Player));
+		}
+		else 
+		{
+			player = null;
+			Debug.Log("UI Error: Sphere: Player game object not found!");
+		}
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player != null){
+			int speedtmp = -1*(int)Mathf.Round(player.currentSpeed);
+			transform.Rotate(transform.up,speedtmp);
+		}
+		
+	}
+}
