675ecfd6b0596a3b439ec2d0d46221a6a6e6b69d
Tanbouz <phoenixsts@gmail.com>
2014-01-13 13:47:26 +0200

    Dynamic flame particles for space ship
    Depends on speed, acceleration and ship turn direction.

---
+++ Assets/scripts/Particles/ShipFlame.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipFlame : MonoBehaviour {
+
+
+	private ParticleSystem particleSys;
+	private Player player;
+
+	// Use this for initialization
+	void Start () {
+
+		particleSys = GetComponent<ParticleSystem>().particleSystem;
+
+		GameObject playerObj = GameObject.Find("Player3D");
+		if (playerObj != null) 
+		{
+			player = (Player)playerObj.GetComponent (typeof(Player));
+		}
+		else 
+		{
+			player = null;
+			Debug.Log("Particles Error: Flame particle: Player game object not found!");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player != null){
+			particleSys.startLifetime = 0.1f+player.currentSpeed/140f + player.currentAcceleration/40f;
+			particleSys.gravityModifier = Input.GetAxisRaw ("Vertical")/16;
+		}
+
+	}
+}
